0ea20419e14d81b7c85862d4c82a950009d70f48
ShashvatJK <39092550+ShashvatJK@users.noreply.github.com>
2018-05-17 16:08:09 +0530

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -1,16 +1,5 @@
 
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hp
- */
 public class Encryption {
     static Scanner s=new Scanner(System.in);
     //input the number of texts
