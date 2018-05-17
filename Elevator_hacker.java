0a1b5d7597357c1f71201190f33c4ccabadfcac4
ShashvatJK <39092550+ShashvatJK@users.noreply.github.com>
2018-05-18 01:48:05 +0530

    Update Elevator_hacker.java

--- Elevator_hacker.java
+++ Elevator_hacker.java
@@ -1,16 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hp
- */
 public class Elevator_hacker {
     public static void main(String[] args){
         int floors_up=0,floors_down=0;
@@ -168,8 +158,6 @@ public class Elevator_hacker {
                         System.out.println("");
                     }
                     System.out.println("\nDistance_2:"+dist_2);
-                }
-                
-    }
-    
+                }           
+    }    
 }
